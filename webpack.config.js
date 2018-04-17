0f5b0ebca75376e8e97aea0e32116a8964168827
Brooks Benson <brooksbenson01@gmail.com>
2018-04-17 13:14:24 -0600

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './calorie-counter/app.js',
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: '[name].html' }
+          },
+          { loader: 'extract-loader' },
+          { loader: 'html-loader' }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
